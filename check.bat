d23f2975434cd9e2de7bd6000c85827eb0c5731e
Josh Clarence Bunag <66039351+JoshuaBunag@users.noreply.github.com>
2021-03-26 09:37:04 +0800

    Kafka and Zookeeper Configurations

---
+++ check.bat
@@ -0,0 +1,1 @@
+cmd /k .\bin\windows\kafka-console-consumer --bootstrap-server localhost:9092 --topic user --from-beginning
---
+++ run-kafka.bat
@@ -0,0 +1,1 @@
+cmd /k .\bin\windows\kafka-server-start .\config\server.properties
